6244bdffc7259b827fd119a920d01a1d55bf7cee
Roberto Dessì <dessi@netseven.it>
2025-02-28 16:05:04 +0100

    refactoring include

--- update-js-libs.sh
+++ update-js-libs.sh
@@ -10,13 +10,13 @@ declare -a libs=(
   "leaflet (css);/node_modules/leaflet/dist/leaflet.css;/leaflet/"
   "leaflet (images);/node_modules/leaflet/dist/images;/leaflet/"
   "leaflet_providers;/node_modules/leaflet-providers/leaflet-providers.js;/leaflet-providers/"
-  "leaflet_spin;/node_modules/leaflet-spin/leaflet.spin.js;/leaflet-spin/"
+  "leaflet_spin;/node_modules/leaflet-spin/leaflet.spin.min.js;/leaflet-spin/"
   "openlayers;/node_modules/ol/dist/ol.js;/openlayers/"
   "openlayers (css);/node_modules/ol/ol.css;/openlayers/"
   "proj4leaflet;/node_modules/proj4leaflet/src/proj4leaflet.js;/proj4leaflet/"
   "proj4;/node_modules/proj4/dist/proj4.js;/proj4/"
   "spinjs;/node_modules/spin.js/spin.js;/spin.js/"
-  "underscore;/node_modules/underscore/underscore-min.js;/underscore/" 
+  "underscore;/node_modules/underscore/underscore-min.js;/underscore/"
 )
 
 for lib in "${libs[@]}"
